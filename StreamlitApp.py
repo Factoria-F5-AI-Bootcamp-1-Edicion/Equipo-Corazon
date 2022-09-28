56d215401f2c0e82ceea42f535f0f8619141673e
DooFromash <garciaadave@gmail.com>
2022-09-28 14:56:02 +0200

    primer esquema de prediction app

---
+++ StreamlitApp.py
@@ -0,0 +1,140 @@
+import streamlit as st
+import pandas as pd
+import warnings
+from sklearn.linear_model import LogisticRegression
+from sklearn.metrics import accuracy_score
+from sklearn.metrics import roc_auc_score
+from sklearn.metrics import r2_score
+from sklearn.metrics import recall_score
+from sklearn.metrics import precision_score
+from sklearn.metrics import confusion_matrix
+import pickle
+from streamlit_option_menu import option_menu
+
+warnings.filterwarnings("ignore")
+  
+
+st.title("¡Hola! Introduce los datos del nuevo paciente")
+
+
+
+work_type = st.selectbox(
+                'Work Type',
+                ('Private', 'Self-employed', 'Govt_job', "children"))
+
+gender = st.selectbox('Gender',('Male', 'Female'))
+   
+age = st.number_input("Age")
+   
+avg_glucose_level= st.number_input("Glucose Level")
+   
+hypertension = st.radio("Hypertension",('Yes', 'No',))
+if hypertension == 'Yes': 
+            hypertension = 1 
+else: 
+            hypertension = 0
+
+
+heart_disease = st.radio("Heart Disease",('Yes', 'No',))
+if heart_disease == 'Yes': 
+            heart_disease = 1 
+else: 
+            heart_disease = 0
+   
+smoking_status = st.selectbox('Smoking Status',('formerly smoked', 'smokes', 'never smoked','Unknown'))
+
+ever_married = st.radio("Married?",('Yes', 'No',))
+
+
+Residence_type = st.selectbox('Residence',('Urban', 'Rural'))
+
+bmi = st.number_input("bmi Level")
+
+stroke = 0
+
+
+#def teclado():
+    # Mensaje de bienvenida
+    
+    # Metemos datos
+       
+    
+    
+age = int(age)
+bmi = float(bmi)
+avg_glucose_level = float(avg_glucose_level)
+heart_disease = int(heart_disease)
+hypertension = int(hypertension)
+stroke = int(stroke)
+ever_married = 'Yes'
+Residence_type = 'Urban'
+    
+list_variables_predictoras = [[gender,age,hypertension,heart_disease,ever_married,work_type,Residence_type,avg_glucose_level,bmi,smoking_status,stroke]]
+
+
+# se elimina stroke ya que es la variable objetivo
+columns = ['gender','age','hypertension','heart_disease','ever_married','work_type','Residence_type','avg_glucose_level','bmi','smoking_status','stroke']
+
+# se crea dataframe del usuario
+df_usuario_test = []
+df_usuario_test = pd.DataFrame(list_variables_predictoras, columns=columns)
+
+# df en crudo
+#print(df_usuario_test.head())
+#print(f"columnas", df_usuario_test.columns )
+#print (df_usuario_test)
+
+st.write(df_usuario_test)
+
+df = df_usuario_test
+
+df["hypertension"] = df["hypertension"].astype(bool)
+df["heart_disease"] = df["heart_disease"].astype(bool)
+df["stroke"] = df["stroke"].astype(bool)
+df["stroke"].value_counts()
+
+
+df.isnull().sum(axis = 0)
+
+
+categoricas = ["gender", "ever_married", "work_type", "Residence_type", "smoking_status", "hypertension", "heart_disease", "stroke"]
+numericas = ["age", "avg_glucose_level", "bmi"]
+
+
+## se elimina variable objetivo, por que es la que queremos predecir
+X = df.drop("stroke", axis=1)
+y = df["stroke"]
+
+
+
+X.head()
+y.head()
+
+
+categoricas = ["gender", "ever_married", "work_type", "Residence_type", "smoking_status", "hypertension", "heart_disease"]
+
+    
+carga_transformer = pickle.load(open('transformer_entrenado.pkl', 'rb'))    
+carga_modelo = pickle.load(open('modelo_entrenado.pkl', 'rb'))
+
+transformer = carga_transformer
+model = carga_modelo
+df = transformer.transform(df)
+
+print(f"Matriz: \n {df}")
+
+predict=model.predict(df)
+
+
+if st.button('Predecir'):
+    st.metric(label="Probabilidad de Ictus", value=predict, delta="30%")
+
+
+
+
+
+
+
+
+
+
